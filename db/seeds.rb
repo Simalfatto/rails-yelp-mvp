cfee2ac76c806c401c0bea74c5aa4b9a0fb77d46
Simon Malfatto <simon.malfatto@gmail.com>
2023-03-15 16:13:19 +0100

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create!({ name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" })
+Restaurant.create!({ name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" })
+Restaurant.create!({ name: "Chez toto", address: "3 rue du bac Paris", category: "french" })
+Restaurant.create!({ name: "Palais des délices", address: "avenue de la gauffre", category: "belgian" })
+Restaurant.create!({ name: "Covid place", address: "rue de la toux", category: "french" })
